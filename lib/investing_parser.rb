ba79c7e3434efa0b15e64bdf091a2a7bf65e2099
denis <denisdenis9331@gmail.com>
2026-03-29 01:22:14 +0500

    f

--- lib/investing_parser.rb
+++ lib/investing_parser.rb
@@ -288,7 +288,12 @@ module TradingLogic
       {
         'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
                         '(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-        'Accept-Language' => 'ru,en;q=0.9'
+        'Accept-Language' => 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Referer' => 'https://ru.investing.com/',
+        'DNT' => '1',
+        'Connection' => 'keep-alive',
+        'Upgrade-Insecure-Requests' => '1'
       }
     end
 
@@ -303,7 +308,7 @@ module TradingLogic
     end
 
     def retriable_status?(code)
-      [429, 503].include?(code)
+      [403, 429, 503].include?(code)
     end
 
     def success_status?(code)
